7abacd94d1a4faf9c45c0984e6c59119a241c773
Tapan Potdar <potdartapan@gmail.com>
2026-01-02 00:11:43 -0500

    fixed function

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -2,3 +2,7 @@ output "website_url" {
   description = "The URL of the static website"
   value       = azurerm_storage_account.storage.primary_web_endpoint
 }
+
+output "function_url" {
+  value = "https://${azurerm_windows_function_app.function_app.default_hostname}/api/GetVisitorCount"
+}
