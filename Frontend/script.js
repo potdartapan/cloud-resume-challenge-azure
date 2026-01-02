8d004e92ed3819ecdedaf06069798885ecc8c32d
Tapan Potdar <potdartapan@gmail.com>
2026-01-02 00:33:51 -0500

    fixed js

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'https://tp-resume-function-f6c0ccbxhsftcyc8.eastus-01.azurewebsites.net/api/GetVisitorCount'; // Change this to your Azure URL later!
+const functionApi = 'https://tf-clouf-resume-challenge-tp.azurewebsites.net/api/GetVisitorCount'; // Change this to your Azure URL later!
 
 const getVisitCount = () => {
     let count = 30; // Default/fallback
