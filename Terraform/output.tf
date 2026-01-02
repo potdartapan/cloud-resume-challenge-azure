94843fd19e65ea1895d5988c0d5c42a6ebc20d47
Tapan Potdar <potdartapan@gmail.com>
2026-01-02 00:23:03 -0500

    fixed terraform and function

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,8 +1,4 @@
-output "website_url" {
-  description = "The URL of the static website"
-  value       = azurerm_storage_account.storage.primary_web_endpoint
-}
-
 output "function_url" {
-  value = "https://${azurerm_windows_function_app.function_app.default_hostname}/api/GetVisitorCount"
+  # Change 'windows' to 'linux' to match your resource declaration
+  value = "https://${azurerm_linux_function_app.function_app.default_hostname}/api/GetVisitorCount"
 }
